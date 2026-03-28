4bff4f00de000ef1a592c9c8a1959234dfb95dd2
Gary <aviwowoyos059@gmail.com>
2026-03-29 01:07:40 +0800

    PlayerState Rack RackSlot TileBag GameState

--- src/main/java/com/kotva/domain/model/GameState.java
+++ src/main/java/com/kotva/domain/model/GameState.java
@@ -1,5 +1,52 @@
 package com.kotva.domain.model;
 
-public class GameState {
+import java.util.List;
 
+public class GameState {
+    private Board board;
+    private TileBag tileBag;
+    private List<PlayerState> players;
+    private int currentPlayerIndex;
+
+    public GameState(List<PlayerState> players) {
+        this.players = players;
+        this.board = new Board();
+        this.tileBag = new TileBag();
+        this.currentPlayerIndex = 0;
+    }
+
+    public Board getBoard(){
+        return board;
+    }
+
+    public TileBag getTileBag(){
+        return tileBag;
+    }
+
+    public List<PlayerState> getPlayers() {
+        return players;
+    }
+
+    public int getCurrentPlayerIndex(){
+        return currentPlayerIndex;
+    }
+
+    public PlayerState getCurrentPlayer() {
+        return players.get(currentPlayerIndex);
+    }
+
+    public void nextTurn(){
+        currentPlayerIndex =(currentPlayerIndex + 1) % players.size();
+    }
+
+    public void initialDraw(){
+        for(int i = 0;i<players.size();i++){
+            PlayerState player = players.get(i);
+            for(int j=0;j<7;j++){
+                Tile newTile = tileBag.drawTile();
+                player.getRack().setTileAt(j, newTile);
+
+            }
+        }
+    }
 }
---
+++ src/main/java/com/kotva/domain/model/TileBag.java
@@ -0,0 +1,62 @@
+package com.kotva.domain.model;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class TileBag {
+    private static final String LETTER_pool =
+            "AAAAAAAAABBCCDDDDEEEEEEEEEEEEFFGGGHHIIIIIIIIIJKLLLLMMNNNNNNOOOOOOOOOPPQRRRRRRSSSSTTTTTTUUUUVVWWXYYZ";
+    private final Random random = new Random();
+
+    private int getScoreForLetter(char letter) {
+
+        switch (letter) {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'L':
+            case 'N':
+            case 'S':
+            case 'T':
+            case 'R':
+                return 1;
+            case 'D':
+            case 'G':
+                return 2;
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                return 3;
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                return 4;
+            case 'K':
+                return 5;
+            case 'J':
+            case 'X':
+                return 8;
+            case 'Q':
+            case 'Z':
+                return 10;
+            default:
+                return 0;
+
+        }
+    }
+
+    public Tile drawTile() {
+        int index = random.nextInt(LETTER_pool.length());
+        char c = LETTER_pool.charAt(index);
+        int score = getScoreForLetter(c);
+
+        Tile tile = new Tile(UUID.randomUUID().toString(), c, score, false);
+
+        return tile;
+    }
+}
