d66687a8d2ffd44550d43b378e17baf8827f2bbe
Gary <aviwowoyos059@gmail.com>
2026-04-03 15:03:56 +0800

    identify position

--- src/main/java/com/kotva/domain/model/Position.java
+++ src/main/java/com/kotva/domain/model/Position.java
@@ -7,7 +7,9 @@ import java.util.Objects;
  * It properly overrides equals() and hashCode() so we can easily check if two
  * positions point to the exact same spot, or use them safely in Lists and Maps.
  */
-//position是final吗
+//position是final吗   是 因为Position是固定的，代码后面还要用于哈希查找（为了在100个里面找的时候更快），
+// 如果玩家把牌从 (7,7) 移到了 (7,8)，代码里的做法是直接给这块牌换一个新的对象：
+// this.position = new Position(7, 8);，而不是去修改旧坐标
 
     public class Position {
         private final int row;
